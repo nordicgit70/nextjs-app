61c226349e287066fa9f5503c07c635e3d647fb5
nordicgit70 <nordicgit70@users.noreply.github.com>
2023-11-21 13:24:01 +0100

    Masonry unstable_generateUtilityClasses() error.

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,44 @@
 import { Metadata } from 'next'
-import { getData } from "./server"
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Masonry from '@mui/lab/Masonry';
+
+const heights = [150, 30, 90, 70, 110, 150, 130, 80, 50, 90, 100, 150, 30, 50, 80];
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(0.5),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export async function generateMetadata(
   { params},
 ): Promise<Metadata> {
-  console.log('--- generateMetaData ---')
-  const res = await getData()
   return {
     title: 'nextjs-app',
   }
 }
 
-
 /** Add your relevant code here for the issue to reproduce */
 export default async function Home() {
-  console.log('--- Client ---')
-  const res = await getData()
-  return null
+  return (
+    <Box sx={{ width: 500, minHeight: 393 }}>
+      <Masonry
+        columns={4}
+        spacing={2}
+        defaultHeight={450}
+        defaultColumns={4}
+        defaultSpacing={1}
+      >
+        {heights.map((height, index) => (
+          <Item key={index} sx={{ height }}>
+            {index + 1}
+          </Item>
+        ))}
+      </Masonry>
+    </Box>
+  );
 }
