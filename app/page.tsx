3a015d2b8a8e9145753598bdb1720842e4ff6699
nordicgit70 <nordicgit70@users.noreply.github.com>
2023-12-01 06:23:17 +0100

    Clean-up dependencies.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,4 @@
 import { Metadata } from 'next'
-import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Masonry from '@mui/lab/Masonry';
-
-const heights = [150, 30, 90, 70, 110, 150, 130, 80, 50, 90, 100, 150, 30, 50, 80];
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(0.5),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
 
 export async function generateMetadata(
   { params},
@@ -24,21 +10,6 @@ export async function generateMetadata(
 
 /** Add your relevant code here for the issue to reproduce */
 export default async function Home() {
-  return (
-    <Box sx={{ width: 500, minHeight: 393 }}>
-      <Masonry
-        columns={4}
-        spacing={2}
-        defaultHeight={450}
-        defaultColumns={4}
-        defaultSpacing={1}
-      >
-        {heights.map((height, index) => (
-          <Item key={index} sx={{ height }}>
-            {index + 1}
-          </Item>
-        ))}
-      </Masonry>
-    </Box>
+  return (<div>Hello world.</div>
   );
 }
